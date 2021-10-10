d1eb3371deed8850a4e824c57e0ff099957d26a7
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-10-10 03:19:39 -0700

    test and options data

---
+++ constants/navData.js
@@ -0,0 +1,29 @@
+export const navOptionsData = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
+
+export const navFavoritesData = [
+  {
+    id: "123",
+    icon: "home",
+    location: "Home",
+    destination: "Code Street, London, UK",
+  },
+  {
+    id: "456",
+    icon: "briefcase",
+    location: "Work",
+    destination: "London Eye, London, UK",
+  },
+];
---
+++ constants/rides.js
@@ -0,0 +1,20 @@
+export const uberRidesData = [
+  {
+    id: "Uber-X-123",
+    title: "Uber X",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
